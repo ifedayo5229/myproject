508936e68088b8e72845aee26ae2437793aa983e
unknown <ademodiseun@gmail.com>
2020-06-10 10:20:56 -0700

    add myproject

---
+++ AnotherArea/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace AnotherArea
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Radius;
+            double Circle;
+            Console.WriteLine("Enter Radius");
+            Radius = Convert.ToInt32(Console.ReadLine());
+            Circle = Math.PI * Radius * Radius;
+
+            Console.WriteLine("The area of the circle of the given radius is:"+Circle);
+        }
+    }
+}
---
+++ AreaofaCircle/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace AreaofaCircle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double radius, area;
+            Console.WriteLine("Enter Radius: ");
+            radius = Convert.ToDouble(Console.ReadLine());
+            area = Math.PI * radius * radius; // pi r square
+
+            Console.WriteLine("\nThe Area of your Circle is:"+area);
+          //  Console.ReadKey();
+
+        }
+        
+    }
+}
